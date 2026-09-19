a3a0ea499a3007e824485392f607e62e0b7c4d32
Benoit Freslon <contact@benoitfreslon.com>
2026-09-19 22:49:40 +0200

    First commit

---
+++ Assets/3DGamekitLite/Packages/Interactive/Runtime/Actions/PlayCustscene.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using Cinemachine;
+using Gamekit3D.GameCommands;
+
+public class PlayCutscene : GameCommandHandler
+{
+    [Header("Cutscene Settings")]
+    public CinemachineVirtualCamera virtualCamera;
+    public float cutsceneDuration = 3f;
+    public int highPriority = 20;
+
+    private int originalPriority;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // ⚠ EXACTEMENT comme SimpleTransformer : tout faire au Awake APRES base.Awake()
+        if (virtualCamera == null)
+            virtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+        virtualCamera.enabled = false;
+        Debug.Log("[PlayCutscene] Registered with interactionType = " + interactionType);
+    }
+
+    public override void PerformInteraction()
+    {
+        Debug.Log("[PlayCutscene] PerformInteraction RECEIVED !");
+
+        if (virtualCamera == null)
+        {
+            Debug.LogWarning("[PlayCutscene] No virtual camera found.");
+            return;
+        }
+
+        virtualCamera.enabled = true;
+
+
+        // Sauvegarde et override de la priorité
+        originalPriority = virtualCamera.Priority;
+        virtualCamera.Priority = highPriority;
+
+        StartCoroutine(CutsceneCoroutine());
+    }
+
+    private IEnumerator CutsceneCoroutine()
+    {
+        yield return new WaitForSeconds(cutsceneDuration);
+
+        // Restaure la priorité originale
+        virtualCamera.Priority = originalPriority;
+
+        Debug.Log("[PlayCutscene] Cutscene finished, priority restored.");
+    }
+}
---
+++ Assets/3DGamekitLite/Scripts/Game/Helpers/CameraSettings.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+namespace Gamekit3D
+{
+    public class CameraSettings : MonoBehaviour
+    {
+        public enum InputChoice
+        {
+            KeyboardAndMouse, Controller,
+        }
+
+        [Serializable]
+        public struct InvertSettings
+        {
+            public bool invertX;
+            public bool invertY;
+        }
+
+
+        public Transform follow;
+        public Transform lookAt;
+        public CinemachineFreeLook keyboardAndMouseCamera;
+        public CinemachineFreeLook controllerCamera;
+        public InputChoice inputChoice;
+        public InvertSettings keyboardAndMouseInvertSettings;
+        public InvertSettings controllerInvertSettings;
+        public bool allowRuntimeCameraSettingsChanges;
+
+        public CinemachineFreeLook Current
+        {
+            get { return inputChoice == InputChoice.KeyboardAndMouse ? keyboardAndMouseCamera : controllerCamera; }
+        }
+
+        void Reset()
+        {
+            Transform keyboardAndMouseCameraTransform = transform.Find("KeyboardAndMouseFreeLookRig");
+            if (keyboardAndMouseCameraTransform != null)
+                keyboardAndMouseCamera = keyboardAndMouseCameraTransform.GetComponent<CinemachineFreeLook>();
+
+            Transform controllerCameraTransform = transform.Find("ControllerFreeLookRig");
+            if (controllerCameraTransform != null)
+                controllerCamera = controllerCameraTransform.GetComponent<CinemachineFreeLook>();
+
+            PlayerController playerController = FindObjectOfType<PlayerController>();
+            if (playerController != null && playerController.name == "Ellen")
+            {
+                follow = playerController.transform;
+
+                lookAt = follow.Find("HeadTarget");
+
+                if (playerController.cameraSettings == null)
+                    playerController.cameraSettings = this;
+            }
+        }
+
+        void Awake()
+        {
+            UpdateCameraSettings();
+            SetupCinemachineInputOverride();
+        }
+
+        void Update()
+        {
+            if (allowRuntimeCameraSettingsChanges)
+            {
+                UpdateCameraSettings();
+            }
+            
+            // Appliquer l'input avec sensibilité à Cinemachine
+            ApplySensitivityToCinemachine();
+        }
+
+        void SetupCinemachineInputOverride()
+        {
+            // Désactiver l'input automatique de Cinemachine pour le contrôler manuellement
+            if (keyboardAndMouseCamera != null)
+            {
+                keyboardAndMouseCamera.m_XAxis.m_InputAxisName = "";
+                keyboardAndMouseCamera.m_YAxis.m_InputAxisName = "";
+            }
+            
+            if (controllerCamera != null)
+            {
+                controllerCamera.m_XAxis.m_InputAxisName = "";
+                controllerCamera.m_YAxis.m_InputAxisName = "";
+            }
+        }
+
+        void ApplySensitivityToCinemachine()
+        {
+            if (PlayerInput.Instance == null)
+                return;
+
+            Vector2 cameraInput = PlayerInput.Instance.CameraInput;
+            
+            CinemachineFreeLook currentCam = Current;
+            if (currentCam != null)
+            {
+                // Appliquer l'input avec la sensibilité déjà intégrée
+                currentCam.m_XAxis.m_InputAxisValue = cameraInput.x;
+                currentCam.m_YAxis.m_InputAxisValue = cameraInput.y;
+            }
+        }
+
+        void UpdateCameraSettings()
+        {
+            keyboardAndMouseCamera.Follow = follow;
+            keyboardAndMouseCamera.LookAt = lookAt;
+            keyboardAndMouseCamera.m_XAxis.m_InvertInput = keyboardAndMouseInvertSettings.invertX;
+            keyboardAndMouseCamera.m_YAxis.m_InvertInput = keyboardAndMouseInvertSettings.invertY;
+
+            controllerCamera.m_XAxis.m_InvertInput = controllerInvertSettings.invertX;
+            controllerCamera.m_YAxis.m_InvertInput = controllerInvertSettings.invertY;
+            controllerCamera.Follow = follow;
+            controllerCamera.LookAt = lookAt;
+
+            keyboardAndMouseCamera.Priority = inputChoice == InputChoice.KeyboardAndMouse ? 1 : 0;
+            controllerCamera.Priority = inputChoice == InputChoice.Controller ? 1 : 0;
+        }
+    } 
+}
